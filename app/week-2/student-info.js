ba852e23ec21e0f024f5000921d3221c7df1d2f9
GabrielOlivr <131859764+GabrielOlivr@users.noreply.github.com>
2024-09-20 14:02:16 -0600

    week-2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+
+const StudentInfo = () => {
+    return (
+        <div>
+            <h1>Gabriel de Oliveira</h1>
+            <p>
+            <Link href="https://github.com/GabrielOlivr/cprg306-assignmnets" target="_blank" rel="noopener noreferrer">
+            GitHub Link 
+            </Link>
+            </p>
+        </div>
+    );
+};
+
+export default StudentInfo;
+
+
+
