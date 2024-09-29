56a78da1b6f362882b89413c2d4fbee70e75520d
GabrielOlivr <131859764+GabrielOlivr@users.noreply.github.com>
2024-09-29 00:26:22 -0600

    .

--- app/page.js
+++ app/page.js
@@ -10,6 +10,7 @@ export default function HomePage() {
       <ul className="text-lg">
         <li><Link href="./week-2/" className={linkStyles}>Week 2 Assignment</Link></li>
         <li><Link href="./week-3/" className={linkStyles}>Week 3 Assignment</Link></li>
+        <li><Link href="./week-4/" className={linkStyles}>Week 4 Assignment</Link></li>
       </ul>
     </main>
   );
